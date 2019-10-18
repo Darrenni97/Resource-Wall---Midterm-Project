8138ca27b90bafb89e2fe9569bf0a21344eb7f87
Darrenni97 <Darrenni_@hotmail.com>
2019-10-18 15:13:53 -0700

    created database tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,45 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  profile_picture VARCHAR(255) NOT NULL DEFAULT 'https://static.thenounproject.com/png/538846-200.png',
+  bio TEXT,
+);
+
+DROP TABLE IF EXISTS pins CASCADE;
+
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  resource_url VARCHAR(255),
+  photo_url VARCHAR(255),
+  tag VARCHAR (15)
+);
+
+DROP TABLE IF EXISTS comments CASCADE;
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  body VARCHAR(139)
+);
+
+DROP TABLE IF EXISTS ratings CASCADE;
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  rating SMALLINT
+);
+
+DROP TABLE IF EXISTS likes CASCADE;
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE
 );
