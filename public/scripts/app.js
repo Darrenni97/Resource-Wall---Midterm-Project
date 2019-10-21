49e9925b5d6932696a261dd89f63a9a3be583e2b
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-20 23:52:24 -0700

    changed location of load pins functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,42 @@ $(() => {
     }
   });;
 });
+
+// Loading pins to the home page
+
+// const loadTweets = function() {
+//   $.ajax({method: 'GET', url: '/tweets', dataType: 'JSON'})
+//     .then(res => {
+//       console.log('Success: ', renderTweets(res));
+//     });
+// };
+
+const loadPins = function() {
+  return db.query(`
+  SELECT title, description, resource_url, photo_url
+  FROM pins
+  `)
+  .then(res => {
+    console.log(res.rows)
+    console.log('Success: ', renderPins(res.rows))
+  })
+  // .then(res => res.rows[0])
+  .catch(err => console.error('query error: user = null', err.stack));
+};
+loadPins()
+// Loading pins to the home page
+const createPinElement = function(pinObject) {
+  return $('#wrapper').append(
+    `<div class="box">
+      <img src="${pinObject.photo_url}"/>
+      <h4>${pinObject.title}</h4>
+      <p>${pinObject.description}</p>
+    </div>`);
+};
+const renderPins = function(pins) {
+  $('#wrapper').empty();
+  for (let i = 0; i < pins.length; i++) {
+    let newPin = createPinElement(pins[i]);
+    $('#wrapper').prepend(newPin);
+  }
+};
