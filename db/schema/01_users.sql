05eeb9154d8a9c81ea6fa02046e64294010589b6
Brandon <brandonjamiller@hotmail.com>
2019-10-24 17:29:08 +0000

    updated pages to hide contents when logged in or not

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   profile_picture VARCHAR(255) NOT NULL DEFAULT 'https://static.thenounproject.com/png/538846-200.png',
-  bio TEXT
+  bio TEXT DEFAULT 'Go to Update Profile to write your own bio!'
 );
 
 DROP TABLE IF EXISTS pins CASCADE;
