aaa69eb39022e26da8333df4a728433390c1b20f
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-24 10:33:31 -0700

    started importing functions to helpers folder

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app        = express();
 const morgan     = require('morgan');
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
+const { FUNCTIONS } = require('./helpers');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
